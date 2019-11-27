bfbf335ea47b35f5d9714b5ae68b76e689eabed4
Robert Lemke <robert@flownative.com>
2019-11-27 08:32:24 +0100

    Initial commit

---
+++ Classes/RedisBackend.php
@@ -0,0 +1,561 @@
+<?php
+declare(strict_types=1);
+
+namespace Flownative\RedisSentinel;
+
+/*
+ * This file is part of the Flownative.RedisSentinel package.
+ *
+ * Copyright (c) 2019 Robert Lemke, Flownative GmbH
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Neos\Cache\Backend\AbstractBackend as IndependentAbstractBackend;
+use Neos\Cache\Backend\FreezableBackendInterface;
+use Neos\Cache\Backend\IterableBackendInterface;
+use Neos\Cache\Backend\PhpCapableBackendInterface;
+use Neos\Cache\Backend\RequireOnceFromValueTrait;
+use Neos\Cache\Backend\TaggableBackendInterface;
+use Neos\Cache\Backend\WithStatusInterface;
+use Neos\Cache\EnvironmentConfiguration;
+use Neos\Cache\Exception as CacheException;
+use Neos\Error\Messages\Error;
+use Neos\Error\Messages\Notice;
+use Neos\Error\Messages\Result;
+use Predis;
+use RuntimeException;
+
+/**
+ * Class RedisBackend
+ */
+class RedisBackend extends IndependentAbstractBackend implements TaggableBackendInterface, IterableBackendInterface, FreezableBackendInterface, PhpCapableBackendInterface, WithStatusInterface
+{
+    use RequireOnceFromValueTrait;
+
+    public const MIN_REDIS_VERSION = '2.6.0';
+
+    /**
+     * @var Predis\Client
+     */
+    protected $client;
+
+    /**
+     * @var integer Cursor used for iterating over cache entries
+     */
+    protected $entryCursor = 0;
+
+    /**
+     * @var boolean|null
+     */
+    protected $frozen;
+
+    /**
+     * @var string
+     */
+    protected $hostname = '127.0.0.1';
+
+    /**
+     * @var integer
+     */
+    protected $port = 6379;
+
+    /**
+     * @var array
+     */
+    protected $sentinels = [];
+
+    /**
+     * @var string
+     */
+    protected $service;
+
+    /**
+     * @var integer
+     */
+    protected $database = 0;
+
+    /**
+     * @var string
+     */
+    protected $password = '';
+
+    /**
+     * @var integer
+     */
+    protected $compressionLevel = 0;
+
+    /**
+     * Constructs this backend
+     *
+     * @param EnvironmentConfiguration $environmentConfiguration
+     * @param array $options Configuration options - depends on the actual backend
+     */
+    public function __construct(EnvironmentConfiguration $environmentConfiguration, array $options)
+    {
+        parent::__construct($environmentConfiguration, $options);
+        $this->client = $this->getRedisClient();
+    }
+
+    /**
+     * Saves data in the cache.
+     *
+     * @param string $entryIdentifier An identifier for this specific cache entry
+     * @param string $data The data to be stored
+     * @param array $tags Tags to associate with this cache entry. If the backend does not support tags, this option can be ignored.
+     * @param integer $lifetime Lifetime of this cache entry in seconds. If NULL is specified, the default lifetime is used. "0" means unlimited lifetime.
+     * @throws RuntimeException
+     * @throws CacheException
+     * @return void
+     * @api
+     */
+    public function set(string $entryIdentifier, string $data, array $tags = [], int $lifetime = null): void
+    {
+        if ($this->isFrozen()) {
+            throw new RuntimeException(sprintf('Cannot add or modify cache entry because the backend of cache "%s" is frozen.', $this->cacheIdentifier), 1574776976);
+        }
+
+        $this->client->multi();
+        $lifetime = $lifetime ?? $this->defaultLifetime;
+        if ($lifetime >0) {
+            $status = $this->client->set($this->buildKey('entry:' . $entryIdentifier), $this->compress($data), 'ex', $lifetime);
+        } else {
+            $status = $this->client->set($this->buildKey('entry:' . $entryIdentifier), $this->compress($data));
+        }
+
+        if ($status === false) {
+            $this->verifyRedisVersionIsSupported();
+        }
+        $this->client->lRem($this->buildKey('entries'), $entryIdentifier, 0);
+        $this->client->rPush($this->buildKey('entries'), [$entryIdentifier]);
+
+        foreach ($tags as $tag) {
+            $this->client->sAdd($this->buildKey('tag:' . $tag), [$entryIdentifier]);
+            $this->client->sAdd($this->buildKey('tags:' . $entryIdentifier), [$tag]);
+        }
+        $this->client->exec();
+    }
+
+    /**
+     * Loads data from the cache.
+     *
+     * @param string $entryIdentifier An identifier which describes the cache entry to load
+     * @return mixed The cache entry's content as a string or false if the cache entry could not be loaded
+     * @api
+     */
+    public function get(string $entryIdentifier)
+    {
+        return $this->decompress($this->client->get($this->buildKey('entry:' . $entryIdentifier)));
+    }
+
+    /**
+     * Checks if a cache entry with the specified identifier exists.
+     *
+     * @param string $entryIdentifier An identifier specifying the cache entry
+     * @return boolean true if such an entry exists, false if not
+     * @api
+     */
+    public function has(string $entryIdentifier): bool
+    {
+        return (bool)$this->client->exists($this->buildKey('entry:' . $entryIdentifier));
+    }
+
+    /**
+     * Removes all cache entries matching the specified identifier.
+     * Usually this only affects one entry but if - for what reason ever -
+     * old entries for the identifier still exist, they are removed as well.
+     *
+     * @param string $entryIdentifier Specifies the cache entry to remove
+     * @throws RuntimeException
+     * @return boolean true if (at least) an entry could be removed or false if no entry was found
+     * @api
+     */
+    public function remove(string $entryIdentifier): bool
+    {
+        if ($this->isFrozen()) {
+            throw new RuntimeException(sprintf('Cannot remove cache entry because the backend of cache "%s" is frozen.', $this->cacheIdentifier), 1323344192);
+        }
+        do {
+            $tagsKey = $this->buildKey('tags:' . $entryIdentifier);
+            $this->client->watch($tagsKey);
+            $tags = $this->client->sMembers($tagsKey);
+            $this->client->multi();
+            $this->client->del([$this->buildKey('entry:' . $entryIdentifier)]);
+            foreach ($tags as $tag) {
+                $this->client->sRem($this->buildKey('tag:' . $tag), $entryIdentifier);
+            }
+            $this->client->del([$this->buildKey('tags:' . $entryIdentifier)]);
+            $this->client->lRem($this->buildKey('entries'), $entryIdentifier, 0);
+            /** @var array|bool $result */
+            $result = $this->client->exec();
+        } while ($result === false);
+
+        return true;
+    }
+
+    /**
+     * Removes all cache entries of this cache
+     *
+     * The flush method will use the EVAL command to flush all entries and tags for this cache
+     * in an atomic way.
+     *
+     * @throws RuntimeException
+     * @return void
+     * @api
+     */
+    public function flush(): void
+    {
+        $script = "
+        local entries = redis.call('LRANGE',KEYS[1],0,-1)
+        for k1,entryIdentifier in ipairs(entries) do
+            redis.call('DEL', ARGV[1]..'entry:'..entryIdentifier)
+            local tags = redis.call('SMEMBERS', ARGV[1]..'tags:'..entryIdentifier)
+            for k2,tagName in ipairs(tags) do
+                redis.call('DEL', ARGV[1]..'tag:'..tagName)
+            end
+            redis.call('DEL', ARGV[1]..'tags:'..entryIdentifier)
+        end
+        redis.call('DEL', KEYS[1])
+        redis.call('DEL', KEYS[2])
+        ";
+        $this->client->eval($script, 2, $this->buildKey('entries'), $this->buildKey('frozen'), $this->buildKey(''));
+        $this->frozen = null;
+    }
+
+    /**
+     * This backend does not need an externally triggered garbage collection
+     *
+     * @return void
+     * @api
+     */
+    public function collectGarbage(): void
+    {
+    }
+
+    /**
+     * @param string $identifier
+     * @return string
+     */
+    private function buildKey(string $identifier): string
+    {
+        return $this->cacheIdentifier . ':' . $identifier;
+    }
+
+    /**
+     * Removes all cache entries of this cache which are tagged by the specified tag.
+     *
+     * @param string $tag The tag the entries must have
+     * @throws RuntimeException
+     * @return integer The number of entries which have been affected by this flush
+     * @api
+     */
+    public function flushByTag(string $tag): int
+    {
+        if ($this->isFrozen()) {
+            throw new RuntimeException(sprintf('Cannot add or modify cache entry because the backend of cache "%s" is frozen.', $this->cacheIdentifier), 1574777747);
+        }
+
+        $script = "
+        local entries = redis.call('SMEMBERS', KEYS[1])
+        for k1,entryIdentifier in ipairs(entries) do
+            redis.call('DEL', ARGV[1]..'entry:'..entryIdentifier)
+            local tags = redis.call('SMEMBERS', ARGV[1]..'tags:'..entryIdentifier)
+            for k2,tagName in ipairs(tags) do
+                redis.call('SREM', ARGV[1]..'tag:'..tagName, entryIdentifier)
+            end
+            redis.call('DEL', ARGV[1]..'tags:'..entryIdentifier)
+            redis.call('LREM', KEYS[2], 0, entryIdentifier)
+        end
+        return #entries
+        ";
+        return $this->client->eval($script, 2, $this->buildKey('tag:' . $tag), $this->buildKey('entries'), $this->buildKey(''));
+    }
+
+    /**
+     * Finds and returns all cache entry identifiers which are tagged by the
+     * specified tag.
+     *
+     * @param string $tag The tag to search for
+     * @return string[] An array with identifiers of all matching entries. An empty array if no entries matched
+     * @api
+     */
+    public function findIdentifiersByTag(string $tag): array
+    {
+        return $this->client->sMembers($this->buildKey('tag:' . $tag));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function current()
+    {
+        return $this->get($this->key());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function next()
+    {
+        $this->entryCursor++;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function key()
+    {
+        $entryIdentifier = $this->client->lIndex($this->buildKey('entries'), $this->entryCursor);
+        if ($entryIdentifier !== false && !$this->has($entryIdentifier)) {
+            return false;
+        }
+        return $entryIdentifier;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function valid(): bool
+    {
+        return $this->key() !== false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rewind()
+    {
+        $this->entryCursor = 0;
+    }
+
+    /**
+     * Freezes this cache backend.
+     *
+     * All data in a frozen backend remains unchanged and methods which try to add
+     * or modify data result in an exception thrown. Possible expiry times of
+     * individual cache entries are ignored.
+     *
+     * A frozen backend can only be thawn by calling the flush() method.
+     *
+     * @throws RuntimeException
+     * @return void
+     */
+    public function freeze(): void
+    {
+        if ($this->isFrozen()) {
+            throw new RuntimeException(sprintf('Cannot add or modify cache entry because the backend of cache "%s" is frozen.', $this->cacheIdentifier), 1574777766);
+        }
+        do {
+            $entriesKey = $this->buildKey('entries');
+            $this->client->watch($entriesKey);
+            $entries = $this->client->lRange($entriesKey, 0, -1);
+            $this->client->multi();
+            foreach ($entries as $entryIdentifier) {
+                $this->client->persist($this->buildKey('entry:' . $entryIdentifier));
+            }
+            $this->client->set($this->buildKey('frozen'), '1');
+            /** @var array|bool $result */
+            $result = $this->client->exec();
+        } while ($result === false);
+        $this->frozen = true;
+    }
+
+    /**
+     * Tells if this backend is frozen.
+     *
+     * @return boolean
+     */
+    public function isFrozen(): bool
+    {
+        if (null === $this->frozen) {
+            $this->frozen = (bool)$this->client->exists($this->buildKey('frozen'));
+        }
+        return $this->frozen;
+    }
+
+    /**
+     * Sets the hostname or the socket of the Redis server
+     *
+     * @param string $hostname Hostname of the Redis server
+     * @api
+     */
+    public function setHostname(string $hostname): void
+    {
+        $this->hostname = $hostname;
+    }
+
+    /**
+     * Sets the port of the Redis server.
+     *
+     * Unused if you want to connect to a socket (i.e. hostname contains a /)
+     *
+     * @param integer|string $port Port of the Redis server
+     * @api
+     */
+    public function setPort($port): void
+    {
+        $this->port = (int)$port;
+    }
+
+    /**
+     * Set the connection addresses for Redis Sentinel servers.
+     *
+     * If at least one Sentinel server is specified, this client operates in Sentinel mode
+     * and ignores "hostname" and "port".
+     *
+     * @param array $sentinels Sentinel server addresses, eg. ['tcp://10.101.213.145:26379', 'tcp://…']
+     */
+    public function setSentinels(array $sentinels): void
+    {
+        $this->sentinels = $sentinels;
+    }
+
+    /**
+     * @param string $service
+     */
+    public function setService(string $service): void
+    {
+        $this->service = $service;
+    }
+
+    /**
+     * Sets the database that will be used for this backend
+     *
+     * @param integer|string $database Database that will be used
+     * @api
+     */
+    public function setDatabase($database): void
+    {
+        $this->database = (int)$database;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @param integer|string $compressionLevel
+     */
+    public function setCompressionLevel($compressionLevel): void
+    {
+        $this->compressionLevel = (int)$compressionLevel;
+    }
+
+    /**
+     * @param Predis\Client $client
+     * @return void
+     */
+    public function setClient(Predis\Client $client = null): void
+    {
+        if ($client !== null) {
+            $this->client = $client;
+        }
+    }
+
+    /**
+     * @param string|bool $value
+     * @return string|bool
+     */
+    private function decompress($value)
+    {
+        if (empty($value)) {
+            return false;
+        }
+        return $this->useCompression() ? gzdecode((string) $value) : $value;
+    }
+
+    /**
+     * @param string $value
+     * @return string
+     */
+    private function compress(string $value): string
+    {
+        return $this->useCompression() ? gzencode($value, $this->compressionLevel) : $value;
+    }
+
+    /**
+     * @return boolean
+     */
+    private function useCompression(): bool
+    {
+        return $this->compressionLevel > 0;
+    }
+
+    /**
+     * @return void
+     * @throws CacheException
+     */
+    protected function verifyRedisVersionIsSupported(): void
+    {
+        return;
+        $serverInfo = (array)$this->client->info('SERVER');
+        if (!isset($serverInfo['redis_version'])) {
+            throw new CacheException('Unsupported Redis version, the Redis cache backend needs at least version ' . self::MIN_REDIS_VERSION, 1574777825);
+        }
+        if (version_compare($serverInfo['redis_version'], self::MIN_REDIS_VERSION) < 0) {
+            throw new CacheException('Redis version ' . $serverInfo['redis_version'] . ' not supported, the Redis cache backend needs at least version ' . self::MIN_REDIS_VERSION, 1574777829);
+        }
+    }
+
+    /**
+     * Validates that the configured redis backend is accessible and returns some details about its configuration if that's the case
+     *
+     * @return Result
+     * @api
+     */
+    public function getStatus(): Result
+    {
+        $result = new Result();
+        return $result;
+        try {
+            $this->verifyRedisVersionIsSupported();
+        } catch (CacheException $exception) {
+            $result->addError(new Error($exception->getMessage(), (int)$exception->getCode(), [], 'Redis Version'));
+            return $result;
+        }
+        $serverInfo = (array)$this->client->info('SERVER');
+        if (isset($serverInfo['redis_version'])) {
+            $result->addNotice(new Notice((string)$serverInfo['redis_version'], null, [], 'Redis version'));
+        }
+        if (isset($serverInfo['tcp_port'])) {
+            $result->addNotice(new Notice((string)$serverInfo['tcp_port'], null, [], 'TCP Port'));
+        }
+        if (isset($serverInfo['uptime_in_seconds'])) {
+            $result->addNotice(new Notice((string)$serverInfo['uptime_in_seconds'], null, [], 'Uptime (seconds)'));
+        }
+        return $result;
+    }
+
+    /**
+     * @return Predis\Client
+     */
+    private function getRedisClient(): \Predis\Client
+    {
+        if ($this->sentinels !== []) {
+            $connectionParameters = $this->sentinels;
+            $options   = [
+                'replication' => 'sentinel',
+                'service' => $this->service,
+                'parameters' => [
+                    'password' => $this->password,
+                    'database' => $this->database
+                ]
+            ];
+        } else {
+            $connectionParameters = 'tcp://' . $this->hostname . ':' . $this->port;
+            $options   = [
+                'parameters' => [
+                    'password' => $this->password,
+                    'database' => $this->database
+                ]
+            ];
+        }
+        return new Predis\Client($connectionParameters, $options);
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+[![MIT license](http://img.shields.io/badge/license-MIT-brightgreen.svg)](http://opensource.org/licenses/MIT)
+[![Packagist](https://img.shields.io/packagist/v/flownative/redis-sentinel.svg)](https://packagist.org/packages/flownative/redis-sentinel)
+[![Maintenance level: Love](https://img.shields.io/badge/maintenance-%E2%99%A1%E2%99%A1%E2%99%A1-ff69b4.svg)](https://www.flownative.com/en/products/open-source.html)
+
+# Flow Redis Cache Backend with Sentinel Support
+
+This package provides a Redis cache backend with Sentinel support.
+
+## Installation
+
+The package is installed as a regular Flow package via Composer. For your existing project,
+simply include `flownative/redis-sentinel` into the dependencies of your Flow or Neos distribution:
+
+```bash
+    $ composer require flownative/redis-sentinel
+```
+
+## Usage
+
+tbd.
